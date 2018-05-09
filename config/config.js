34d3dd5336f1ad952176cbe1103986ee66291f05
Berkay GURSOY <berkaygursoy@gmail.com>
2018-05-09 23:02:56 +0300

    remove config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "localmongoDB": "mongodb://localhost/DavinciDB",
-    "remotemongoDB":"mongodb://admin:admin12345@ds161939.mlab.com:61939/davinci",
+    "remotemongoDB":"mongoRemoteUrl",
     "secret": "danvincisecretkey",
     "tokenExpireTime": "7 Days",
     "thumbnailQuality":80,
